f60cb61dfa9d9b7de557f817df0132545ccc298d
Robert Herber <robert@herber.me>
2018-01-20 23:23:01 -0500

    Fixed issue where metro doesnt find index.ios.js

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-export class Action {
-  constructor(opts, onComplete) {
-    throw new Error('Only iOS is supported by react-native-ios-notification-actions');
-  }
-}
+import Default from './react-native-ios-notification-actions';
 
-export class Category {
+export * from './react-native-ios-notification-actions';
 
-  constructor(opts) {
-    throw new Error('Only iOS is supported by react-native-ios-notification-actions');
-  }
-
-}
-
-export const updateCategories = (categories, shouldRequestPermission = true) => {
-  throw new Error('Only iOS is supported by react-native-ios-notification-actions');
-};
-
-
-export default {
-  Action,
-  Category,
-  updateCategories
-};
+export default Default;
